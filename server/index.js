51d33a2358b328a146b57400fc1e5d2dcde4a7df
Rudr2004 <rudrajan24@gmail.com>
2024-11-19 19:16:17 +0530

    Add files

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const connect = require("./config/db.js");
+const router = require("./router/index.js");
+const cookiesParser = require("cookie-parser");
+const { app, server } = require("./socket/index.js");
+require("dotenv").config();
+
+//const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookiesParser());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running on " + PORT });
+});
+
+//Api End Points
+app.use("/api", router);
+
+connect().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+});
