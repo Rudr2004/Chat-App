4b68faf613d0f18a01aa9aba867331f8983de47a
Rudr2004 <rudrajan24@gmail.com>
2024-11-21 15:48:30 +0530

    update

--- frontend/src/components/Pages/checkPassword.jsx
+++ frontend/src/components/Pages/checkPassword.jsx
@@ -61,7 +61,7 @@ const CheckPasswordPage = () => {
             setData({
               password : "",
             })
-            navigate('/home')
+            navigate('/')
         }
     } catch (error) {
         toast.error(error?.response?.data?.message)
