cc6a23c13324fe4e73ec7a2263dd26f410a977d3
Rudr2004 <rudrajan24@gmail.com>
2024-11-22 22:52:18 +0530

    socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -31,10 +31,10 @@ io.on("connection", async (socket) => {
   const user = await getUserDetailsFromToken(token);
 
   //create a room
-  socket.join(user?._id);
+  socket.join(user?._id.toString());
   onlineUser.add(user?._id?.toString());
   if (user && user._id) {
-    socket.join(user._id);
+    socket.join(user._id.toString());
     onlineUser.add(user._id.toString());
   } else {
     console.error("Error: User or User ID is undefined", { user });
