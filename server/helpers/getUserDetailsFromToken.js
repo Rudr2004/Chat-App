920757bd93ffb76a182e3384de2e7f3d38b2a341
Rudr2004 <rudrajan24@gmail.com>
2024-11-24 15:32:46 +0530

    user

--- server/helpers/getUserDetailsFromToken.js
+++ server/helpers/getUserDetailsFromToken.js
@@ -9,11 +9,16 @@ const getUserDetailsFromToken = async (token) => {
     };
   }
 
-  const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-  const user = await UserModel.findById(decode.id).select("-password");
-
-  return user;
+  try {
+    const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await UserModel.findById(decode.id).select("-password");
+    return user;
+  } catch (error) {
+    return {
+      message: "Invalid token",
+      logout: true,
+    };
+  }
 };
 
 module.exports = getUserDetailsFromToken;
